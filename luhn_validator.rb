c8e102324e88a4998120d0d24c574c8c446bfd0e
YKGio <aresr417@gmail.com>
2023-03-09 14:52:35 +0800

    update LuhnValidator

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -3,9 +3,20 @@ module LuhnValidator
   # arguments: none
   # assumes: a local String called 'number' exists
   # returns: true/false whether last digit is correct
-  def validate_checksum
-    nums_a = number.to_s.chars.map(&:to_i)
+  def luhn(digit, index)
+    res = index.odd? ? digit * 2 : digit
+    res.to_s.chars.map(&:to_i).sum
+  end
 
+  def validate_checksum
+    digit_list = number.to_s.chars.map(&:to_i)
+    digit_list_rev = digit_list.reverse
+
+    sr_code = digit_list_rev.each_with_index.map { |digit, index| luhn(digit, index) }
+
+    check_sum = sr_code.sum
+
+    (check_sum % 10).zero?
     # TODO: use the integers in nums_a to validate its last check digit
   end
 end
